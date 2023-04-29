2bd7d5f6f6126f1301416d66a5e451fa700bc354
kolya <kybiyevych97@gmail.com>
2023-04-29 19:03:25 +0300

    hw05-avatars

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -5,7 +5,7 @@ const ctrl = require("../../controllers/auth")
 
 const {validateBody} = require("../../utils");
 
-const {authenticate} = require("../../middlewares");
+const {authenticate, upload, resizeAvatar} = require("../../middlewares");
 
 const {schemas} = require("../../models/users");
 
@@ -22,4 +22,7 @@ router.get("/current", authenticate, ctrl.getCurrent);
 router.post("/logout", authenticate, ctrl.logout);
 //
 router.patch("/", authenticate, validateBody(schemas.updateSubscription), ctrl.updateSubscription )
+
+router.patch("/avatars", authenticate, upload.single("avatar"), resizeAvatar, ctrl.updateAvatar)
+
 module.exports = router;
